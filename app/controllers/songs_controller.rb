d7973890b6790e09faea22a59bb404e01d3d21e8
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-07-22 23:32:42 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -38,6 +38,7 @@ post '/songs' do
     @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
     @song.genre_ids = params[:genres]
     @song.save
+    
    redirect("/songs/#{@song.slug}")
   end
 end
