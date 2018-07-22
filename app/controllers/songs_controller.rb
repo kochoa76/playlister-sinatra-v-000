f05eed7e981c5b2b11e30f45d1af6146ba6fdfd5
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-07-22 23:27:16 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -22,7 +22,7 @@ post '/songs' do
   @song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
   @song.genre_ids = params[:genres]
   @song.save
-  @song_slug=Song.find_by_slug(params[:slug])
+  
 
   redirect to "/songs/#{@song_slug.slug}"
   end
