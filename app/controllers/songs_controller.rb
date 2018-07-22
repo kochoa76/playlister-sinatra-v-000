b52c90de3d7e11c1a1a88cbcb426dd2461cdb328
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-07-22 23:32:24 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -36,9 +36,8 @@ post '/songs' do
     @song = Song.find_by_slug(params[:slug])
     @song.update(params[:song])
     @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
-    @song.genre_ids= params[:genres]
+    @song.genre_ids = params[:genres]
     @song.save
-
     redirect to "/songs/#{@song.slug}"
   end
 end
