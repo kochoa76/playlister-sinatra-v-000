312db5d033519560fa056389e74730f46976ab4b
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-07-22 23:34:15 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,10 +4,9 @@ class SongsController < ApplicationController
   # use Rack::Flash
 
   get '/songs' do
-  @songs = Song.all
-  erb :'/songs/index'
-end
-
+    @songs = Song.all
+    erb :'/songs/index'
+  end
 get '/songs/new' do
     erb :'/songs/new'
   end
